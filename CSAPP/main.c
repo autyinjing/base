9029e9ce607ae6fe20b78bb1888b27c68a807bc2
autyinjing <autyinjing@126.com>
2017-06-01 16:12:36 +0800

    add test

---
+++ CSAPP/main.c
@@ -0,0 +1,15 @@
+/* ***********************************************************************
+
+  > File Name: main.c
+  > Author: Aut
+  > Mail: aut@taomee.com 
+  > Created Time: 2017年06月01日 星期四 11时53分33秒
+
+ ********************************************************************** */
+
+int sum(int x, int y);
+
+int main(int argc, const char* argv[])
+{
+    return sum(1, 3);
+}
